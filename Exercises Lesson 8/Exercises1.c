c9e525cb12060f4548741b4c3d06ab3a7b2cd383
Trieu Than <trieuthan92pro@gmail.com>
2021-09-13 16:10:12 +0700

    Add update exercises l08

--- Exercises Lesson 8/Exercises1.c
+++ Exercises Lesson 8/Exercises1.c
@@ -9,9 +9,9 @@ int main() {
 	scanf("%d", &n); // đọc n vào
 	// kiểm tra
 	if (n % 2 == 0) { // n chia hết cho 2 thì n là số chẵn
-		printf("%d la so chan.\n", n); // \n để in xuống dòng mới
+		printf("EVEN\n"); // \n để in xuống dòng mới
 	}
 	else { // ngược lại n là số lẻ
-		printf("%d la so le\n", n);
+		printf("ODD\n");
 	}
 }
