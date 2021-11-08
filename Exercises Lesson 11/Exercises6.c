9adeac302c4f1d231174862ccd9868b8a4c8f22c
Trieu Than <trieuthan92pro@gmail.com>
2021-11-08 21:14:18 +0700

    Add update ex6-10 l11

--- Exercises Lesson 11/Exercises6.c
+++ Exercises Lesson 11/Exercises6.c
@@ -1,24 +1,24 @@
-﻿// Nhập số nguyên dương n sau đó tính tổng S = 1 + 2! + ... + n!
 #include <stdio.h>
-/*
- * @author Branium Academy
-*/
-int main() {
-	int n;
-	puts("Nhap so tu nhien n > 0: ");
-	scanf("%d", &n);
 
-	if (n > 0) { // nếu n duong thì mới thực hiện
-		unsigned long long s = 0;
-		unsigned long long f = 1;
-		int i;
-		for (i = 1; i <= n; i++) {
-			f *= i; // k!, k = i
-			s += f; // cộng từng thành phần k!
-		}
-		printf("S = %llu\n", s);
-	}
-	else {
-		puts("Vui long nhap so tu nhien n > 0");
-	}
+int main() {
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++) {
+        int n;
+        scanf("%d", &n);
+        if(n >= 1 && n <= 20) {
+            unsigned long long sum = 0;
+            unsigned long long factorial = 1;
+            for (int j = 1; j <= n; j++) {
+                factorial *= j;
+                sum += factorial;
+            }
+            printf("Test %d:\n%llu\n", i, sum);
+        } else {
+            printf("Test %d:\nINVALID", i);
+        }
+    }
+    puts("");
+    
+    return 0;
 }
