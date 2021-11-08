d529d38c960b0742d858addfe0bbc604cb3539ab
Trieu Than <trieuthan92pro@gmail.com>
2021-11-08 11:46:37 +0700

    Add exercises lesson 11 v2021.11.08

---
+++ Exercises Lesson 11/Exercises11_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++) {
+        int n, k;
+        scanf("%d%d", &n, &k);
+        if(n < 0 || k > n) {
+            puts("NO RESULT");
+        } else {
+            for (int j = k; j <= n; j++) {
+                if(j % 2 == 1) {
+                    printf("%d ", j);
+                }
+            }
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
