0beb51e27ac696e62f73c570e22056957b5b0cd2
Trieu Than <trieuthan92pro@gmail.com>
2022-02-15 17:01:15 +0700

    Add exercises 8-10 lesson 12

---
+++ Exercises Lesson 12/Exercises9.c
@@ -0,0 +1,30 @@
+﻿/*
+ * @author Branium Academy
+ * @version 2022.02.15
+ * @see https://braniumacademy.net
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int t, a, b, k;
+	scanf("%d", &t);
+	while (t > 0) {
+		scanf("%d%d%d", &a, &b, &k);
+		if (a > b || k > b) {
+			printf("\n"); // không có kết quả
+		}
+		else {
+			int i = a;
+			while (i <= b) {
+				if (i % k == 0) {
+					printf("%d ", i);
+				}
+				i++;
+			}
+			printf("\n");
+		}
+		t--;
+	}
+}
