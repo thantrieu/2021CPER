616390ab60d5ce755ccbda3725c2be69f298c097
Trieu Than <trieuthan92pro@gmail.com>
2022-02-15 17:01:15 +0700

    Add exercises 8-10 lesson 12

---
+++ Exercises Lesson 12/Exercises10.c
@@ -0,0 +1,31 @@
+﻿/*
+ * @author Branium Academy
+ * @version 2022.02.15
+ * @see https://braniumacademy.net
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int t, n;
+	scanf("%d", &t);
+	int counter = 1;
+	while (t > 0) {
+		scanf("%d", &n);
+		int other = 0;
+		int tmp = n;
+		while (tmp > 0) {
+			other = other * 10 + tmp % 10;
+			tmp /= 10;
+		}
+		if (n == other) {
+			printf("Test %d: YES\n", counter);
+		}
+		else {
+			printf("Test %d: NO\n", counter);
+		}
+		t--;
+		counter++;
+	}
+}
---
+++ Exercises Lesson 12/Exercises8.c
@@ -0,0 +1,40 @@
+﻿/*
+ * @author Branium Academy
+ * @version 2022.02.15
+ * @see https://braniumacademy.net
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int t, a, b;
+	scanf("%d", &t);
+	while (t > 0) {
+		scanf("%d%d", &a, &b);
+		if (a > b) {
+			printf("\n");
+		}
+		else {
+			for (int i = a; i <= b; i++) {
+				int isPrime = 1;
+				if (i < 2) {
+					isPrime = 0;
+				}
+				int bound = (int)sqrt(i);
+				int j = 2;
+				while (j <= bound) {
+					if (i % j == 0) {
+						isPrime = 0;
+					}
+					j++;
+				}
+				if (isPrime == 1) {
+					printf("%d ", i);
+				}
+			}
+			printf("\n");
+		}
+		t--;
+	}
+}
