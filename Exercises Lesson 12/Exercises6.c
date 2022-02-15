caccaeba2400325eeb88f111ce9673c1cb022e81
Trieu Than <trieuthan92pro@gmail.com>
2022-02-15 16:30:50 +0700

    Update ex6+7 lesson 12

--- Exercises Lesson 12/Exercises6.c
+++ Exercises Lesson 12/Exercises6.c
@@ -1,36 +1,41 @@
-﻿// Nhập số nguyên dương a, b và kiểm tra xem chúng có phải là
-// số nguyên tố cùng nhau hay không
-#include <stdio.h>
-/*
+﻿/*
  * @author Branium Academy
+ * @version 2022.02.15
+ * @see https://braniumacademy.net
 */
-int main() {
-	int a, b;
-	puts("Nhap 2 so nguyen duong a, b: ");
-	scanf("%d%d", &a, &b);
 
-	if (a <= 0 || b <= 0) {
-		puts("Vui long nhap hai so nguyen duong a, b");
-	}
-	else {
-		// số nguyên tố cùng nhau là cặp số có ước chung lớn nhất
-		// bằng 1
-		// tìm ước chung lớn nhất của hai số a, b
-		while (a != b) {
-			if (a > b) {
-				a -= b;
-			}
-			else {
-				b -= a;
-			}
-		}
-		// lúc này a = b nên ta kiểm tra a hoặc b đều ok
-		
-		if (a == 1) {
-			puts("Hai so nguyen to cung nhau");
+#include <stdio.h>
+
+int main() {
+	int t; // số lượng bộ test
+	int a, b;
+	scanf("%d", &t);
+	while (t > 0) {
+		scanf("%d%d", &a, &b);
+		if (a * b <= 0 || a < 0) { // giả định không xét số âm và 2 số có tích bằng 0
+			puts("NO");
 		}
 		else {
-			puts("Hai so khong nguyen to cung nhau");
+			// số nguyên tố cùng nhau là cặp số có ước chung lớn nhất
+			// bằng 1
+			// tìm ước chung lớn nhất của hai số a, b
+			while (a != b) {
+				if (a > b) {
+					a -= b;
+				}
+				else {
+					b -= a;
+				}
+			}
+			// lúc này a = b nên ta kiểm tra a hoặc b đều ok
+
+			if (a == 1) {
+				puts("YES");
+			}
+			else {
+				puts("NO");
+			}
 		}
+		t--;
 	}
 }
