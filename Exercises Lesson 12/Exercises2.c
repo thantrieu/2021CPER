62f4cfb000a895177d796e7d9cb19cb1e94b709a
Trieu Than <trieuthan92pro@gmail.com>
2022-02-15 15:48:38 +0700

    Add update ex2+3 l12

--- Exercises Lesson 12/Exercises2.c
+++ Exercises Lesson 12/Exercises2.c
@@ -1,24 +1,25 @@
-﻿// Tính tổng bình phương các chữ số của n
-
-#include <stdio.h>
-/*
- * @author Branium Academy
+﻿/**
+* @author Branium Academy
+* @version 2022.02.15
+* @see https://braniumacademy.net
 */
+#include <stdio.h>
+
 int main() {
-	int n;
-	puts("Nhap so nguyen duong n: ");
-	scanf("%d", &n);
-	if (n < 0) {
-		puts("Vui long nhap so nguyen duong n");
-	}
-	else {
-		int sum = 0;
-		while (n > 0) {
-			int digit = n % 10; // lấy chữ số phần đơn vị của n
-			sum += digit * digit; // cộng bình phương chữ số vào sum
-			n /= 10; // giảm n đi 10 lần để loại bỏ phần đơn vị đã lấy
+	int t; // số lượng bộ test
+	int n; // số n cần tách các chữ số và tính tổng
+	scanf("%d", &t); // đọc vào số bộ test
+	int counter = 1; // bắt đầu đếm số bộ test
+	while (t > 0) { // lặp t lần
+		scanf("%d", &n); // đọc vào số n
+		int sum = 0; // khởi tạo biến tổng bằng 0
+		while (n > 0) { // lặp chừng nào n còn > 0
+			int digit = n % 10; // tách phần đơn vị 
+			sum += digit * digit; // bình phương phần đơn vị và cộng dồn vào tổng
+			n /= 10; // loại bỏ chữ số phần đơn vị đã sử dụng
 		}
-		// lúc này có kết quả rồi, in ra thôi
-		printf("Sum = %d\n", sum);
+		printf("Test %d: \n%d\n", counter, sum); // in ra kết quả
+		counter++; // tăng số đếm các bộ test
+		t--;
 	}
 }
