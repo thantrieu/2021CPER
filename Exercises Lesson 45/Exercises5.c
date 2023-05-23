99b73fffde99f4f1cc25897a564ea6b51c442736
Than Trieu <34931034+thantrieu@users.noreply.github.com>
2023-05-23 16:02:43 +0700

    Update Exercises5.c

--- Exercises Lesson 45/Exercises5.c
+++ Exercises Lesson 45/Exercises5.c
@@ -1,11 +1,14 @@
-﻿// Bài tập đọc ghi file text số 4
+// Bài tập đọc ghi file text số 5
+#define _CRT_SECURE_NO_WARNINGS // nếu không dùng visual studio thì xóa bỏ dòng này đi
 #include <stdio.h>
 
 /*
 * @author Branium Academy
+* @version 2023.05
+* @see https://braniumacademy.net
 */
 
-// cấu trúc sinh viên
+// cấu trúc mô tả thông tin một sinh viên
 typedef struct {
 	char id[20];		// mã sinh viên
 	char fullName[40];	// họ tên đầy đủ
@@ -13,55 +16,59 @@ typedef struct {
 	float math;			// điểm toán
 	float eng;			// điểm tiếng anh
 	float prog;			// điểm lập trình
-	float avgGrade;	// điểm TB
+	float gpa;			// điểm TB
 } Student;
 
 // hàm nguyên mẫu
 // hàm đọc file
-void readFile(Student* students, size_t* n);
+void readFile(Student* students, int* n);
 // hàm ghi file
-void writeFile(const Student* students, size_t n, const char* fileName);
+void writeFile(const Student* students, int n, const char* fileName);
 // hàm hiển thị
-void showStudents(const Student* students, size_t n);
+void showStudents(const Student* students, int n);
 // hàm sắp xếp theo điểm TB giảm dần
-void sort(Student* students, size_t n);
+void sort(Student* students, int n);
 // hàm tính điểm trung bình
-void calculAvgGrade(Student* students, size_t n);
+void calculGpa(Student* students, int n);
 
 int main() {
 	Student students[100];
-	size_t n;
+	int n;
 	char fileName[40];
 	puts("Nhap ten file dau ra: ");
 	scanf("%39s", fileName);
 
 	// hàm đọc file
 	readFile(students, &n);
+
 	// hàm tính toán điểm TB
-	calculAvgGrade(students, n);
-	// hàm hiển thị danh sách sv
-	showStudents(students, n);
+	calculGpa(students, n);
+
 	// hàm sắp xếp danh sách theo điểm TB giảm dần
 	sort(students, n);
+
+	// hàm hiển thị danh sách sv
+	showStudents(students, n);
+
 	// hàm ghi thông tin sau xử lý ra file
 	writeFile(students, n, fileName);
 
 	return 0;
 }
 
-void readFile(Student* students, size_t* n) {
+void readFile(Student* students, int* n) {
 	FILE* fin = fopen("STUDENTS.INP", "r"); // mo file de doc
 	if (fin) {
-		fscanf(fin, "%u", n); // doc so luong sv
-		size_t i; 
+		fscanf(fin, "%d%*c", n); // doc so luong sv
+		int i;
 		for (i = 0; i < *n; i++) {
 			// dùng %*c để đọc bỏ kí tự thừa của lần đọc trước đó
-			fscanf(fin, "%*c%19[^-]", students[i].id);
+			fscanf(fin, "%19[^-]", students[i].id);
 			fscanf(fin, "%*c%39[^-]", students[i].fullName);
 			fscanf(fin, "%*c%39[^-]", students[i].address);
-			fscanf(fin, "%*c%f%*c%f%*c%f%*c", &students[i].math, 
+			fscanf(fin, "%*c%f%*c%f%*c%f%*c", &students[i].math,
 				&students[i].eng, &students[i].prog);
-			students[i].avgGrade = 0; // ban dau gan gia tri diem tb = 0
+			students[i].gpa = 0; // ban dau gan gia tri diem tb = 0
 		}
 		fclose(fin);
 	}
@@ -70,15 +77,21 @@ void readFile(Student* students, size_t* n) {
 	}
 }
 
-void writeFile(const Student* students, size_t n, const char* fileName) {
+void writeFile(const Student* students, int n, const char* fileName) {
 	FILE* fout = fopen(fileName, "w"); // mo file de ghi
 	if (fout) {
-		size_t i;
+		fprintf(fout, "===================================="
+			" DANH SACH SINH VIEN "
+			"=====================================\n");
+		fprintf(fout, "%-15s%-20s%-20s%-10s%-10s%-10s%-10s\n",
+			"Ma SV", "Ho va ten", "Dia chi", "Toan",
+			"Anh", "Lap trinh", "Diem TB");
+		int i;
 		for (i = 0; i < n; i++) {
-			fprintf(fout, "%s-%s-%s-%0.2f-%0.2f-%0.2f-%0.2f\n",
+			fprintf(fout, "%-15s%-20s%-20s%-10.2f%-10.2f%-10.2f%-10.2f\n",
 				students[i].id, students[i].fullName,
 				students[i].address, students[i].math,
-				students[i].eng, students[i].prog, students[i].avgGrade);
+				students[i].eng, students[i].prog, students[i].gpa);
 		}
 		fclose(fout);
 	}
@@ -87,7 +100,7 @@ void writeFile(const Student* students, size_t n, const char* fileName) {
 	}
 }
 
-void showStudents(const Student* students, size_t n) {
+void showStudents(const Student* students, int n) {
 	if (n > 0) {
 		puts("===================================="
 			" DANH SACH SINH VIEN "
@@ -95,7 +108,7 @@ void showStudents(const Student* students, size_t n) {
 		printf("%-15s%-20s%-20s%-10s%-10s%-10s%-10s\n",
 			"Ma SV", "Ho va ten", "Dia chi", "Toan",
 			"Anh", "Lap trinh", "Diem TB");
-		size_t i;
+		int i;
 		for (i = 0; i < n; i++)
 		{
 			puts("------------------------------"
@@ -104,7 +117,7 @@ void showStudents(const Student* students, size_t n) {
 			printf("%-15s%-20s%-20s%-10.2f%-10.2f%-10.2f%-10.2f\n",
 				students[i].id, students[i].fullName, students[i].address,
 				students[i].math, students[i].eng,
-				students[i].prog, students[i].avgGrade);
+				students[i].prog, students[i].gpa);
 		}
 	}
 	else {
@@ -112,15 +125,15 @@ void showStudents(const Student* students, size_t n) {
 	}
 }
 
-void sort(Student* students, size_t n) {
+void sort(Student* students, int n) {
 	void swap(Student * a, Student * b);
 	if (n > 0) {
-		size_t i, j;
+		int i, j;
 		for (i = 0; i < n - 1; i++)
 		{
 			for (j = n - 1; j > i; j--)
 			{
-				if (students[j].avgGrade > students[j - 1].avgGrade) {
+				if (students[j].gpa > students[j - 1].gpa) {
 					swap(&students[j], &students[j - 1]);
 				}
 			}
@@ -138,11 +151,11 @@ void swap(Student* a, Student* b) {
 	*b = s;
 }
 
-void calculAvgGrade(Student* students, size_t n) {
-	size_t i;
+void calculGpa(Student* students, int n) {
+	int i;
 	for (i = 0; i < n; i++)
 	{
-		students[i].avgGrade = 
+		students[i].gpa =
 			(students[i].math + students[i].eng + students[i].prog) / 3;
 	}
 }
